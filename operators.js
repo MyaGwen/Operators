31402904db5794e293e09329d47019e8a89efd2c
MyaGwen <wendy.e.ahadome@gmail.com>
2023-03-06 04:43:26 -0800

    commit

---
+++ operators.js
@@ -0,0 +1,51 @@
+let num1 = 10;
+let num2 = 5;
+
+// Arithmetic operators
+
+//multiplication
+console.log(num1 * num2);
+
+//Division
+console.log(num1 / num2);
+
+//Addition
+console.log(num1 + num2);
+
+//Subtraction
+console.log(num1 - num2);
+
+//Modulus
+console.log(num1 % num2);
+
+//Using Increment and Decrement
+
+//Increment
+console.log(++num1);
+console.log(++num2);
+
+//Decrement
+console.log(--num1);
+console.log(--num2);
+
+//Using the prompt funtion
+
+let input1 = prompt(parseFloat());
+let input2 = prompt(parseFloat());
+
+// Arithmetic operators based on user's input
+
+//multiplication
+console.log(input1 * input2);
+
+//Division
+console.log(input1 / input2);
+
+//Addition
+console.log(input1 + input2);
+
+//Subtraction
+console.log(input1 - input2);
+
+//Modulus
+console.log(input1 % input2);
